fafc44c2d852c2ba2232f640e587c05fc7640498
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-05 12:49:59 +0100

    Update keys.sh

--- keys.sh
+++ keys.sh
@@ -3,8 +3,8 @@ sudo apt install vim-gtk -y
 sudo apt install curl -y
 
 # import keys
-gpg --import secret.gpg
-gpg --import public.gpg
+gpg --import /home/jim/secret.gpg
+gpg --import /home/jim/public.gpg
 
 # set the keys trust level 
 echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27D578CEDDB5272262D4C trust
@@ -12,29 +12,21 @@ echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27
 shred -u -n 33 -z public.gpg
 shred -u -n 33 -z secret.gpg
 
-# download config files / dot files
-git clone https://github.com/Jimmy-sha256/config_files.git
-git clone https://github.com/Jimmy-sha256/dot_files.git
 
 # transfer config files
-cp /home/jim/config_files/gpg-agent.conf /home/jim/.gnupg 
-cp /home/jim/config_files/sshcontrol /home/jim/.gnupg 
-cp /home/jim/config_files/gtk.css /home/jim/.config/gtk-3.0
+cp /home/jim/ubunt_setup/config_files/gpg-agent.conf /home/jim/.gnupg 
+cp /home/jim/ubunt_setup/config_files/sshcontrol /home/jim/.gnupg 
+cp /home/jim/ubunt_setup/config_files/gtk.css /home/jim/.config/gtk-3.0
 
 # transfer dot files
-cp /home/jim/dot_files/.bash_profile /home/jim/
-cp /home/jim/dot_files/.bashrc /home/jim/
-cp /home/jim/dot_files/.gitconfig /home/jim/
-cp /home/jim/dot_files/.gitignore_global /home/jim/
-cp /home/jim/dot_files/.inputrc /home/jim/
-cp /home/jim/dot_files/.profile /home/jim/
-cp /home/jim/dot_files/.tmux.conf /home/jim/
-cp /home/jim/dot_files/.vimrc /home/jim/
-
-# remove temp directories
-rm -rf /home/jim/config_files
-rm -rf /home/jim/dot_files
-
+cp /home/jim/ubunt_setup/dot_files/.bash_profile /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.bashrc /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.gitconfig /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.gitignore_global /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.inputrc /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.profile /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.tmux.conf /home/jim/
+cp /home/jim/ubunt_setup/dot_files/.vimrc /home/jim/
 
 # dock settings
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
