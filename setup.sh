83057a2266b235376f0910667a800f71524c2710
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-06 21:49:31 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -38,4 +38,6 @@ gsettings set org.gnome.desktop.background picture-uri file:///home/jim/ubuntu_s
 
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-theme-colors false # disable terminal theme colors
 
+rm -rf /home/jim/ubuntu_setup/
+
 reboot
