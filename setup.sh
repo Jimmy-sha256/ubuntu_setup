dc32227f2efe1985aa2345286e0d462369b1b18a
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-06 19:22:29 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -33,9 +33,10 @@ gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
 gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 35
 
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']" # replace caps with ctrl
+gsettings set org.gnome.desktop.session idle-delay 0 # set power saving to never
 
-gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop']"
+gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop']" # edit app thumbnails
 
-dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-theme-colors false
+dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-theme-colors false # disable terminal theme colors
 
 reboot
