fe2e3c26ae6a008997dce5f1ae7138a4e8b51c82
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-06 22:07:22 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -27,7 +27,7 @@ cp /home/jim/ubuntu_setup/dot_files/.profile /home/jim/
 cp /home/jim/ubuntu_setup/dot_files/.tmux.conf /home/jim/
 cp /home/jim/ubuntu_setup/dot_files/.vimrc /home/jim/
 
-cp /home/jim/ubuntu_setup/background.jpg /home/jim/
+cp /home/jim/ubuntu_setup/background.jpg /home/jim/Pictures/
 
 # dock settings
 gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
@@ -36,10 +36,12 @@ gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 35
 gsettings set org.gnome.desktop.input-sources xkb-options "['ctrl:nocaps']" # replace caps with ctrl
 gsettings set org.gnome.desktop.session idle-delay 0 # set power saving to never
 gsettings set org.gnome.shell favorite-apps "['firefox.desktop', 'org.gnome.Terminal.desktop', 'org.gnome.Nautilus.desktop']" # edit app thumbnails
-gsettings set org.gnome.desktop.background picture-uri file:///home/jim/background.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///home/jim/Pictures/background.jpg
 
 dconf write /org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9/use-theme-colors false # disable terminal theme colors
 
 reboot
+
+git clone git@github.com:Jimmy-sha256/wiki.git
 
 sudo rm -rf /home/jim/ubuntu_setup/
