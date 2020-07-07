b0fe185fabeaaf8005fbc6cc7389d0e8becf639c
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-07 14:45:40 +0100

    Update pull_repos.sh

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
+# pull repos
 git clone git@github.com:Jimmy-sha256/wiki.git
 git clone git@github.com:Jimmy-sha256/archive.git
 
+# decrypt the .gpg
 gpg --decrypt /home/jim/archive/archive.gpg > /home/jim/archive.tar.gz
 
+# remove archive repo
 rm -rf /home/jim/archive/
 
+# extract archive
 tar xzf archive.tar.gz
 
+# remove archive.tar.gz
 shred -u -n 33 -z archive.tar.gz
