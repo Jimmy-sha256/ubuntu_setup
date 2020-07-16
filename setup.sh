19421049dd3a1c90cec84377ed3764865ab9ce4b
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-07-16 21:33:18 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ media_keys=org.gnome.settings-daemon.plugins.media-keys
 custom_kbd=org.gnome.settings-daemon.plugins.media-keys.custom-keybinding
 kbd_path=/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/$name/
 new_bindings=`gsettings get $media_keys custom-keybindings | sed -e"s>'\]>','$kbd_path']>"| sed -e"s>@as \[\]>['$kbd_path']>"
-`
+#`
 gsettings set $media_keys custom-keybindings "$new_bindings"
 gsettings set $custom_kbd:$kbd_path name $name
 gsettings set $custom_kbd:$kbd_path binding $binding
