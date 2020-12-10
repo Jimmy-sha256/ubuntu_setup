01a8fd7f20f62c5c3c9d97dd3a3c7398f3b5e786
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-12-10 12:16:34 +0000

    virtualenv

--- pull_repos.sh
+++ pull_repos.sh
@@ -5,6 +5,10 @@ git clone git@github.com:Jimmy-sha256/wiki.git
 git clone git@github.com:Jimmy-sha256/archive.git
 git clone git@github.com:Jimmy-sha256/orders.git
 
+# create venv for orders
+
+virtualenv --python=/usr/bin/python3.7 /home/jim/orders/venv
+
 # decrypt the .gpg
 gpg --decrypt /home/jim/archive/archive.gpg > /home/jim/archive.tar.gz
 
