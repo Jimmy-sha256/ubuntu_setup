2a1c50b9ee330220014a4d0a083d5bfa1ccc40a3
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2020-12-21 10:02:00 +0000

    Update pull_repos.sh

--- pull_repos.sh
+++ pull_repos.sh
@@ -3,13 +3,13 @@
 # pull repos
 git clone git@github.com:Jimmy-sha256/wiki.git
 git clone git@github.com:Jimmy-sha256/archive.git
-git clone git@github.com:Jimmy-sha256/orders.git
+git clone git@github.com:Jimmy-sha256/binance_orders.git
 
 # create venv for orders
 virtualenv --python=/usr/bin/python3.7 /home/jim/orders/venv
 
 # pip install python-binance
-source /home/jim/test/venv/bin/activate
+source /home/jim/binance_orders/venv/bin/activate
 pip3 install python-binance
 deactivate
 
