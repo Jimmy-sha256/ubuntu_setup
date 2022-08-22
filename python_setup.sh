63a981ae192f283c0420f0d6983383e38028cb5c
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2022-08-22 18:37:06 +0100

    Update python_setup.sh

--- python_setup.sh
+++ python_setup.sh
@@ -2,11 +2,11 @@
 
 # Install python3.7
 
-sudo apt install software-properties-common -y
+#sudo apt install software-properties-common -y
 
-sudo add-apt-repository ppa:deadsnakes/ppa
+#sudo add-apt-repository ppa:deadsnakes/ppa
 
-sudo apt install python3.7 -y
+#sudo apt install python3.7 -y
 
 # Install venv
 
