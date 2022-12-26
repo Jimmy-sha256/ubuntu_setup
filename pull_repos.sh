d6f94372c1dd8460ada788de13191024a68b5990
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2022-12-26 13:08:19 +0000

    Update pull_repos.sh

--- pull_repos.sh
+++ pull_repos.sh
@@ -4,14 +4,8 @@
 git clone git@github.com:Jimmy-sha256/encrypted_archive.git
 git clone git@github.com:Jimmy-sha256/trade_calc.git
 
-#git clone git@github.com:Jimmy-sha256/balance.git
-#chmod 777 /home/jim/balance/balance.sh
-
 # decrypt the .gpg
-gpg --decrypt /home/jim/archive/archive.gpg > /home/jim/archive.tar.gz
-
-# remove archive repo
-rm -rf /home/jim/archive/
+gpg --decrypt /home/jim/encrypted_archive/archive.gpg > /home/jim/archive.tar.gz
 
 # extract archive
 tar -xzvf archive.tar.gz
