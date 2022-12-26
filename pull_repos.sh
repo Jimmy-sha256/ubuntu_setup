5c4ab2718f495a7e992f946db62c0b8112976608
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2022-12-26 00:09:24 +0000

    Update pull_repos.sh

--- pull_repos.sh
+++ pull_repos.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
 # pull repos
-# git clone git@github.com:Jimmy-sha256/wiki.git
 git clone git@github.com:Jimmy-sha256/encrypted_archive.git
-#git clone git@github.com:Jimmy-sha256/balance.git
-#git clone git@github.com:Jimmy-sha256/binance.git
-#git clone git@github.com:Jimmy-sha256/deribit_orders.git
 git clone git@github.com:Jimmy-sha256/trade_calc.git
 
-####
-
-#cd /home/jim/balance/ && virtualenv --python=/usr/bin/python3.10 venv && source venv/bin/activate && pip install -r requirements.txt && deactivate && cd ..
-#cd /home/jim/deribit_orders/ && virtualenv --python=/usr/bin/python3.10 venv && source venv/bin/activate && pip install -r requirements.txt && deactivate && cd ..
-#cd /home/jim/binance/ && virtualenv --python=/usr/bin/python3.10 venv && source venv/bin/activate && pip install -r requirements.txt && deactivate && cd ..
-
-
+#git clone git@github.com:Jimmy-sha256/balance.git
 #chmod 777 /home/jim/balance/balance.sh
-#chmod 777 /home/jim/binance_orders/binance.sh
-#chmod 777 /home/jim/deribit_orders/deribit_order.sh
 
 # decrypt the .gpg
 gpg --decrypt /home/jim/archive/archive.gpg > /home/jim/archive.tar.gz
