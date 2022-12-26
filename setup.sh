6ed3fd04d2bc1ee78a174d1e02291c37fcf194e3
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2022-12-26 13:02:19 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -60,17 +60,17 @@ git clone git@github.com:Jimmy-sha256/encrypted_archive.git
 git clone git@github.com:Jimmy-sha256/trade_calc.git
 
 # decrypt the .gpg
-gpg --decrypt /home/jim/encrypted_archive/archive.gpg > /home/jim/archive.tar.gz
+#gpg --decrypt /home/jim/encrypted_archive/archive.gpg > /home/jim/archive.tar.gz
 
 # extract archive
-tar -xzvf archive.tar.gz
+#tar -xzvf archive.tar.gz
 
 # remove archive.tar.gz
-shred -u -n 33 -z archive.tar.gz
+#shred -u -n 33 -z archive.tar.gz
 
 
 # remove setup folder
-sudo rm -rf /home/jim/ubuntu_setup/
+#sudo rm -rf /home/jim/ubuntu_setup/
 
 reboot
 
