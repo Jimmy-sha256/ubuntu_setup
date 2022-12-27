66bbf8d2f8e10f6f9172eb10414072f004123f31
James Alexander <55412677+Jimmy-sha256@users.noreply.github.com>
2022-12-27 10:16:47 +0000

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -18,6 +18,13 @@ gpg --import /media/jim/GPG_SUB_KEYS/public.gpg
 # set the keys trust level 
 echo -e "5\ny\n" |  gpg --command-fd 0 --expert --edit-key D5557B332830404939C27D578CEDDB5272262D4C trust
 
+# create .ssh dir
+mkdir .ssh
+
+# create known_hosts file add github.com
+echo "|1|0i6WxRGmlbEebLZPeII2XIH4g2U=|OS34x2x8Iz3DB/rwRak/R0iikGs= ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl" \
+>> .ssh/known_hosts
+
 # transfer config files
 cp /home/jim/ubuntu_setup/config_files/gpg-agent.conf /home/jim/.gnupg 
 cp /home/jim/ubuntu_setup/config_files/sshcontrol /home/jim/.gnupg 
